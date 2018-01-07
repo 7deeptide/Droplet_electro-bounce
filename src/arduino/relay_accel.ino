eca3cb5e52cc893732b3e8dd57219c3404d65bd4
7deeptide <7deeptide@gmail.com>
2018-01-07 00:37:22 -0800

    added convergence plots, condition estimates, charge decay, and additional notes

---
+++ src/arduino/relay_accel.ino
@@ -0,0 +1,118 @@
+#include <SparkFun_ADXL345.h>         // SparkFun ADXL345 Library
+
+/*********** COMMUNICATION SELECTION ***********/
+/*    Comment Out The One You Are Not Using    */
+//ADXL345 adxl = ADXL345(10);           // USE FOR SPI COMMUNICATION, ADXL345(CS_PIN);
+ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
+
+/****************** INTERRUPT ******************/
+/*      Uncomment If Attaching Interrupt       */
+//int interruptPin = 2;                 // Setup pin 2 to be the interrupt pin (for most Arduino Boards)
+int buttonPin = 8;
+
+/******************** SETUP ********************/
+/*          Configure ADXL345 Settings         */
+int in1 = 7;
+//const
+int buttonPin = 2;     // the number of the pushbutton pin
+
+// variables will change:
+//volatile int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup(){
+  
+  Serial.begin(9600);                 // Start the serial terminal
+  Serial.println("SparkFun ADXL345 Accelerometer Hook Up Guide Example");
+  Serial.println();
+  // initialize the pushbutton pin as an input:
+  //pinMode(buttonPin, INPUT);
+  // Attach an interrupt to the ISR vector
+  
+  pinMode(in1, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  digitalWrite(in1, LOW);
+  
+  adxl.powerOn();                     // Power on the ADXL345
+
+  adxl.setRangeSetting(16);           // Give the range settings
+                                      // Accepted values are 2g, 4g, 8g or 16g
+                                      // Higher Values = Wider Measurement Range
+                                      // Lower Values = Greater Sensitivity
+                                   
+  // Set values for what is considered FREE FALL (0-255)
+  adxl.setFreeFallThreshold(5);       // (5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(20);       // (20 - 70) recommended - 5ms per increment
+ 
+  // Setting all interupts to take place on INT1 pin
+  //adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);" 
+                                                        // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+                                                        // This library may have a problem using INT2 pin. Default to INT1 pin.
+  
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(0);
+  adxl.ActivityINT(0);
+  adxl.FreeFallINT(1);
+  adxl.doubleTapINT(0);
+  adxl.singleTapINT(0);
+  
+//attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+//attachInterrupt(digitalPinToInterrupt(buttonPin), pin_ISR, RISING);
+}
+
+/****************** MAIN CODE ******************/
+/*     Accelerometer Readings and Interrupt    */
+void loop(){
+  
+  // Accelerometer Readings
+  int x,y,z;   
+  adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
+
+  int buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  delay(10);        // delay in between reads for stability
+  if (buttonState < 0.5){
+    digitalWrite(in1, HIGH);
+    //break  
+  }
+  // Output Results to Serial
+  /* UNCOMMENT TO VIEW X Y Z ACCELEROMETER VALUES */  
+  //Serial.print(x);
+  //Serial.print(", ");
+  //Serial.print(y);
+  //Serial.print(", ");
+  //Serial.println(z);
+
+   
+  
+  ADXL_ISR();
+  // You may also choose to avoid using interrupts and simply run the functions within ADXL_ISR(); 
+  //  and place it within the loop instead.  
+  // This may come in handy when it doesn't matter when the action occurs. 
+
+}
+
+/********************* ISR *********************/
+/* Look for Interrupts and Triggered Action    */
+//void pin_ISR() {
+//  digitalWrite(in1, HIGH);
+//}
+
+void ADXL_ISR() {
+  
+  // getInterruptSource clears all triggered actions after returning value
+  // Do not call again until you need to recheck for triggered actions
+  byte interrupts = adxl.getInterruptSource();
+  
+  // Free Fall Detection
+  if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
+    Serial.println("*** FREE FALL ***");
+    //add code here to do when free fall is sensed
+    delay(300);
+    digitalWrite(in1, HIGH);
+  }
+  //else {
+  //  delay(50);
+  //  digitalWrite(in1, HIGH);
+  //} 
+}
+
